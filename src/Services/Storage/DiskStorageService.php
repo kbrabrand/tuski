7f5e8d7d4c97c0569a14ac584868f07f339c4b70
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-22 20:40:25 +0100

    Added empty disk storage service class

---
+++ src/Services/Storage/DiskStorageService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Vaffel\Tuski\Services\Storage;
+
+use Vaffel\Tuski\Interfaces\StorageServiceInterface;
+
+class DiskStorageService implements StorageServiceInterface
+{
+    public function storeChunk($resourceId, $offset, $length)
+    {
+
+    }
+
+    public function getChunk($resourceId, $offset, $length)
+    {
+
+    }
+
+    public function getChunks($resourceId)
+    {
+
+    }
+
+    public function deleteChunks($resourceId)
+    {
+
+    }
+
+}
